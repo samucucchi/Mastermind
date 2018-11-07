a45fd50f6f15d03322d8898dd1c5b76f0911cf57
Rocche <giacomorocchetti97@gmail.com>
2018-11-07 16:22:05 +0100

    Ora i tentativi vengono aggiunti alla schermata

--- Mastermind/src/gui/controller/GameController.java
+++ Mastermind/src/gui/controller/GameController.java
@@ -3,14 +3,15 @@ package gui.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
 public class GameController {
 
+	final int RADIUS = 30;
 	@FXML private Button redPin;
 	@FXML private Button bluePin;
 	@FXML private Button greenPin;
@@ -22,6 +23,8 @@ public class GameController {
 	
 	@FXML private GridPane sequence;
 	
+	@FXML private VBox previousSequences;
+	
 	@FXML
 	private void selectColor(ActionEvent event) {
 		String id = ((Button)event.getSource()).getId();
@@ -29,43 +32,35 @@ public class GameController {
 		switch(id) {
 		case "redPin":
 			color = "red";
-			redPin.setStyle("-fx-background-color: white");
-			redPin.setDisable(true);
+			disablePin(redPin);
 			break;
 		case "bluePin":
 			color = "blue";
-			bluePin.setStyle("-fx-background-color: white");
-			bluePin.setDisable(true);
+			disablePin(bluePin);
 			break;
 		case "greenPin":
 			color = "green";
-			greenPin.setStyle("-fx-background-color: white");
-			greenPin.setDisable(true);
+			disablePin(greenPin);
 			break;
 		case "yellowPin":
 			color = "yellow";
-			yellowPin.setStyle("-fx-background-color: white");
-			yellowPin.setDisable(true);
+			disablePin(yellowPin);
 			break;
 		case "orangePin":
 			color = "orange";
-			orangePin.setStyle("-fx-background-color: white");
-			orangePin.setDisable(true);
+			disablePin(orangePin);
 			break;
 		case "purplePin":
 			color = "purple";
-			purplePin.setStyle("-fx-background-color: white");
-			purplePin.setDisable(true);
+			disablePin(purplePin);
 			break;
 		case "brownPin":
 			color = "brown";
-			brownPin.setStyle("-fx-background-color: white");
-			brownPin.setDisable(true);
+			disablePin(brownPin);
 			break;
 		case "blackPin":
 			color = "black";
-			blackPin.setStyle("-fx-background-color: white");
-			blackPin.setDisable(true);
+			disablePin(blackPin);
 			break;
 		default:
 			System.out.println("Color not recognized");
@@ -78,6 +73,48 @@ public class GameController {
 				pin.setFill(Paint.valueOf(color));
 				break;
 			}
+		}
+	}
+	
+	@FXML
+	private void checkSequence() {
+		for(int i = 0; i < sequence.getChildren().size(); i++) {
+			Circle pin = (Circle)sequence.getChildren().get(i);
+			if(pin.getFill() == Paint.valueOf("white")) {
+				System.out.println("Completare la sequenza");
+				return;
+			}
+		}
+		GridPane previousSequence = createPreviousSequence(sequence);
+		previousSequences.getChildren().add(previousSequence);
+		clearSequence(sequence);
+	}
+	
+	private void disablePin(Button pin) {
+		pin.setStyle("-fx-background-color: white");
+		pin.setDisable(true);
+	}
+	
+	private GridPane createPreviousSequence(GridPane sequence) {
+		GridPane previousSequence = new GridPane();
+		for(int i = 0; i < sequence.getChildren().size(); i++) {
+			Paint pinColor = ((Circle)sequence.getChildren().get(i)).getFill();
+			Circle previousPin = createSequenceCircle(pinColor);
+			previousSequence.add(previousPin, i, 0);
+		}
+		return previousSequence;
+	}
+	
+	private Circle createSequenceCircle(Paint color) {
+		Circle circle = new Circle(RADIUS, color);
+		return circle;
+	}
+	
+	private void clearSequence(GridPane sequence) {
+		Paint whiteColor = Paint.valueOf("white");
+		for(int i = 0; i < sequence.getChildren().size(); i++) {
+			Circle pin = ((Circle)sequence.getChildren().get(i));
+			pin.setFill(whiteColor);
 		}
 	}
 }
