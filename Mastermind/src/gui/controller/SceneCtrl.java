0c9d7b08522d6f6853b293b7d7683f28efa918ea
Rocche <giacomorocchetti97@gmail.com>
2018-11-12 10:04:45 +0100

    Controller variabili a seconda della difficoltà

--- Mastermind/src/gui/controller/SceneCtrl.java
+++ Mastermind/src/gui/controller/SceneCtrl.java
@@ -38,25 +38,36 @@ public class SceneCtrl {
 
 	public static void showGame(String difficulty) throws IOException{
 		String gamePath;
+		GameController controller;
 		switch(difficulty) {
 		case "easy":
 			gamePath = EASY_GAME_PATH;
+			controller = new EasyGameController();
 			break;
 		case "normal":
 			gamePath = NORMAL_GAME_PATH;
+			controller = new NormalGameController();
 			break;
 		case "hard":
 			gamePath = HARD_GAME_PATH;
+			controller = new HardGameController();
 			break;
 		default:
 			throw new IOException("Board not found");
 		}
-		HBox game = (HBox)loadView(gamePath);
+		HBox game = (HBox)loadGameView(gamePath, controller);
 		SceneCtrl.primaryStage.setScene(new Scene(game));
 	}
 	
 	private static Parent loadView(String path) throws IOException {
 		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(SceneCtrl.class.getResource(path));
+		return(loader.load());
+	}
+	
+	private static Parent loadGameView(String path, GameController controller) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setController(controller);
 		loader.setLocation(SceneCtrl.class.getResource(path));
 		return(loader.load());
 	}
