d3605dbfbab7120e7c0a3b953c07ce0ab17b122a
Rocche <giacomorocchetti97@gmail.com>
2018-11-12 19:15:07 +0100

    Messo er

---
+++ Mastermind/src/player/Player.java
@@ -0,0 +1,18 @@
+package player;
+
+public class Player {
+	private String username; 
+	
+	public Player(String username) {
+		this.setUsername(username);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	private void setUsername(String username) {
+		// TODO Auto-generated method stub
+		this.username = username; 
+	}
+}
